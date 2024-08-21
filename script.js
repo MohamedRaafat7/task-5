69601ceeecf0f94a3867d2a5df39f17a7c5d96a6
qweasdf1231 <mraft5868@gmail.com>
2024-08-22 00:28:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector('.b-1').addEventListener('click', function() {
+    document.querySelector('.pp-1').style.backgroundColor = '#f9cf73'; 
+    document.querySelector('.p-a').style.color = '#ffffff'; 
+});
+
+document.querySelector('.b-2').addEventListener('click', function() {
+    document.querySelector('.pp-2').style.backgroundColor = '#6194e2'; 
+    document.querySelector('.p-b').style.color = '#ffffff'; 
+});
+
+document.querySelector('.b-3').addEventListener('click', function() {
+    document.querySelector('.pp-3').style.backgroundColor = '#df7d66'; 
+    document.querySelector('.p-c').style.color = '#ffffff'; 
+});
+
+document.querySelector('.b-4').addEventListener('click', function() {
+    document.querySelector('.pp-4').style.backgroundColor = '#354c6c'; 
+    document.querySelector('.p-d').style.color = '#ffffff'; 
+});
